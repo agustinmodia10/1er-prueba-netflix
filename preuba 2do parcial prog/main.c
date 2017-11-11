70072dc7f1ea14abf37c3da4e6b3bcbfc8a0c406
agustin modia <agustinmodiamoleracing@gmail.com>
2017-11-10 22:00:27 -0300

    hh

--- preuba 2do parcial prog/main.c
+++ preuba 2do parcial prog/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 typedef struct
 {
     char marca [20];
@@ -7,7 +8,7 @@ typedef struct
     int id;
 }ecompu;
 
-
+ecompu* contructorconparametros(char*, char*, int);
 ecompu* contructor();
 
 int main()
@@ -15,12 +16,46 @@ int main()
     ecompu *pc;
     int i;
 
+
+
+   char modelos [1][100]={"rog"};
+
+char marcas [1][100]={"asus"};
+int id = 10;
+
+
+
      pc= contructor();
     for(i=0; i<5; i++ )
     {
         printf(" %s \n  %s \n %d \n\n", pc->marca, pc->modelo, pc->id);
     }
+     pc= contructorconparametros(marcas, modelos, id);
 
+     system("pause");
+
+
+     for(i=0; i<5; i++ )
+    {
+        printf(" %s \n  %s \n %d \n\n", pc->marca, pc->modelo, pc->id);
+    }
+
+
+//hora x sistema
+
+
+        time_t hora;
+
+  time (&hora);
+  printf ("hora local: %s", ctime (&hora));
+
+// cargo valiable atravez del puntero
+
+int num;
+int *puntero;
+puntero = & num;
+* puntero =5;
+printf ("%d %d", num , puntero);
      return 0;
 }
 
@@ -34,6 +69,20 @@ ecompu* contructor()
         pc->id = 0;
         strcpy(pc->marca, " ");
         strcpy(pc->modelo, " ");
+        aux = pc;
+    }
+  return aux;
+}
+
+ecompu* contructorconparametros(char *marca, char * modelo, int id)
+{
+    ecompu *aux = NULL;
+    ecompu *pc = (ecompu*) malloc(sizeof(ecompu));
+    if(pc != NULL)
+    {
+        pc->id = id;
+        strcpy(pc->marca, marca);
+        strcpy(pc->modelo, modelo);
         aux = pc;
     }
   return aux;
